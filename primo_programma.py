21f012589dab731c9b39523e01da2d527276dfd5
riper <riper@DESKTOP-342ODKP.>
2024-10-10 00:17:44 +0200

    first_commit

---
+++ primo_programma.py
@@ -0,0 +1,26 @@
+#Primo programma su python
+nomerobot = 'Pierfranco'
+print('Salve, io sono ' + nomerobot)
+
+username = input('Posso chiederle il suo nome? ')
+print('Salve ' + username)
+
+userage = int(input('Per noleggiare una macchina devi prima inserire la tua eta '))
+if userage >= 18 == True:
+    print('Un ultimo passaggio!')
+else:
+    print('Ripassa tra qualche anno!')
+
+userpatente = input('Hai la patente? ')
+
+si = True
+Si = True
+if userpatente == str(si) == str(Si):
+    print('La tua nuova auto ti aspetta!!!')
+else:
+    print('Torna quando avrai la patente!')
+
+
+
+
+
